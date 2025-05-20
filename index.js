dc5a1c7ee43c4bd37360c1f07c8944c88071ff74
Agency8group <AgencyGroup@naver.com>
2025-05-20 10:42:28 +0900

    test

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// index.js
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
+
+// 루트 경로 응답 추가
+app.get('/', (req, res) => {
+  res.send('서버는 정상 작동 중입니다. /api/hello를 호출해보세요.');
+});
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: '백엔드 연결 성공!' });
